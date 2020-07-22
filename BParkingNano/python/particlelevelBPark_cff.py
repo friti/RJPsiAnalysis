099dc7ce3fe6c05689f2362a5b74cb1eb574ed00
friti <federica.riti@cern.ch>
2020-07-22 15:50:50 +0200

    first commit

--- BParkingNano/python/particlelevelBPark_cff.py
+++
@@ -1,4 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from  PhysicsTools.NanoAOD.particlelevel_cff import *
-
-particleLevelBParkSequence = cms.Sequence(mergedGenParticles + genParticles2HepMC + particleLevel)
